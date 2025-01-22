e003ce919b23b1cf691ba2aeede4bd54d559e651
hlalljie <hlalljie@gmail.com>
2025-01-21 16:16:25 -0800

    Editor setup and show host movement on Player 2 window

---
+++ scenes/Game.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class Game : Node2D
+{
+	public override void _Ready()
+	{
+		var peer = new ENetMultiplayerPeer();
+
+		if (Array.Exists(OS.GetCmdlineArgs(), arg => arg == "--host"))
+		{
+			peer.CreateServer(8910);
+			SpawnPlayer(1);  // Host player
+		}
+		else
+		{
+			peer.CreateClient("localhost", 8910);
+		}
+		// When a new player connects, create their player
+		Multiplayer.PeerConnected += (long id) =>
+		{
+			GD.Print($"Peer {id} connected, spawning their player");
+			SpawnPlayer(id);
+		};
+		// Start using this peer for networking
+		Multiplayer.MultiplayerPeer = peer;
+	}
+
+	private void SpawnPlayer(long id)
+	{
+		var player = GD.Load<PackedScene>("res://scenes/player.tscn").Instantiate<Player>();
+		player.Name = id.ToString();
+		AddChild(player);
+	}
+}
--- scenes/Player.cs
+++ scenes/Player.cs
@@ -4,8 +4,26 @@ using Godot;
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
+	private MultiplayerSynchronizer _sync;
+
+	public override void _Ready()
+	{
+		_sync = GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer");
+		_sync.SetMultiplayerAuthority(Convert.ToInt32(Name));
+	}
+
 
 	public override void _PhysicsProcess(double delta)
+	{
+		// Only process input if we're the authority for this player
+		if (_sync.IsMultiplayerAuthority())
+		{
+			HandleMovement(delta);
+		}
+
+	}
+
+	public void HandleMovement(double delta)
 	{
 		Vector2 velocity = Velocity;
 
