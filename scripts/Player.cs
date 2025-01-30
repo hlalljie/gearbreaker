fa44cb8e80b2724827814a5738ce0c153b8530bf
neo from the matrix ;) <silver_zatch@icloud.com>
2025-01-29 18:42:14 -0800

    modified:   scripts/Player.cs

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -5,6 +5,9 @@ public partial class Player : CharacterBody2D
 {
 	int dint = 0; // DEBUG int for print messages
 
+	//Sprite 1&2 will switch on and off to change state without flickering
+	bool isSprite1 = true;
+	bool spriteSwapped = false; // stops animClock from changing values bc they are being set manually
 	public Sprite2D CharSprite1;
 	public Sprite2D CharSprite2;
 	ActionStateType actionType;
@@ -42,7 +45,9 @@ public partial class Player : CharacterBody2D
 		CharSprite1 = GetNode<Sprite2D>("/root/Game/BossPlayer/Sprite1");
 		CharSprite2 = GetNode<Sprite2D>("/root/Game/BossPlayer/Sprite2");
 		CharSprite1.Visible = false;
+		CharSprite1.Vframes = 5;
 		CharSprite2.Visible = false;
+		CharSprite2.Vframes = 5;
 
 		// DEBUGGING 
 		InitializeAnimation(CharacterType.Dragon);
@@ -55,16 +60,31 @@ public partial class Player : CharacterBody2D
 		if (isInitialized){
 
 			// Detect input and start new action
-			
-			if (!actionQueued && Input.IsKeyPressed(Key.Shift)){ //Bite
+
+			//Bite
+			if (!actionQueued && Input.IsKeyPressed(Key.Shift)){ 
 				actionQueued = true;
 
 				if (actionType != ActionStateType.draBite){
-					GD.Print("Calling bite");
 					SetAnim(ActionStateType.draBite);
 				}
 
 			} else if (actionQueued && Input.IsKeyPressed(Key.Shift) == false){
+				
+				// check if animation needs to play out before returning to movable state
+				actionQueued = false;
+				requestActionCancel = true;
+				
+			}
+			// Claw
+			if (!actionQueued && Input.IsKeyPressed(Key.Z)){ //Bite
+				actionQueued = true;
+
+				if (actionType != ActionStateType.draClaw){
+					SetAnim(ActionStateType.draClaw);
+				}
+
+			} else if (actionQueued && Input.IsKeyPressed(Key.Z) == false){
 				
 				// check if animation needs to play out before returning to movable state
 				actionQueued = false;
@@ -84,19 +104,18 @@ public partial class Player : CharacterBody2D
 		}
 
 		switch(type_){
+
 			case CharacterType.Dragon:
-			
+
 				CharType = type_;
-				SetAnim(ActionStateType.draIdle);
+				SetAnim(ActionStateType.general);
+				isInitialized = true;
 				break;
 
 			default:
 				GD.Print("Tried to initialize animation but " + type_.ToString() + " is not yet supported.");
 				return;
 		}
-
-		CharSprite1.Visible = true;
-		isInitialized = true;
 	}
 
 	void AnimTimer(){
@@ -111,27 +130,32 @@ public partial class Player : CharacterBody2D
 
 		bool actionChanged = false;
 		
-
 		await ToSignal(GetTree().CreateTimer(0.08f), "timeout");
 		
 		HandleAnim();
 
+		// Sprite to handle
+		Sprite2D setSprite;
+		if (isSprite1) setSprite = CharSprite1;
+		else setSprite = CharSprite2;
+
 		if (lockDirection){
 			if (!actionQueued && requestActionCancel){
-				if (animClock + 1 >= CharSprite1.Hframes){
+				
+				if (animClock + 1 >= setSprite.Hframes){
 					actionQueued = false;
 					requestActionCancel = false;
 					lockDirection = false;
 					actionChanged = true;
 					
-					SetAnim(ActionStateType.draWalk);
+					SetAnim(ActionStateType.general);
 				}
 			} 
 		}
 
 		// set sprite frame
 		if (!actionChanged) {
-			if (animClock + 1 >= CharSprite1.Hframes) animClock = 0;
+			if (animClock + 1 >= setSprite.Hframes) animClock = 0;
 			else animClock++;
 		} else animClock = 0;
 
@@ -172,112 +196,273 @@ public partial class Player : CharacterBody2D
 	void HandleAnim(){
 		
 		//DEBUG
-		GD.Print(actionType.ToString() + " " + animClock.ToString());
+		GD.Print(isSprite1 + "\n" + actionType.ToString() + " " + animClock.ToString());
 		//DEBUG
+
+		// Sprite to handle
+		Sprite2D setSprite;
+		if (isSprite1) setSprite = CharSprite1;
+		else setSprite = CharSprite2;
 
 		// is input zero
 		if (direction.X > -0.10f && direction.X < 0.1f && direction.Y > -0.1 && direction.Y < 0.1f){
 			
 			//zero detected :: -0.70710677, -0.70710677
-			if (CharSprite1.FrameCoords.Y == 0){
+			if (setSprite.FrameCoords.Y == 0){
+				 
 				CharSprite1.Frame = animClock;
-			} else
+				CharSprite2.Frame = animClock;
+			} else{
+				 
 				CharSprite1.Frame = CharSprite1.FrameCoords.Y * CharSprite1.Hframes + animClock;
-			
+				CharSprite2.Frame = CharSprite2.FrameCoords.Y * CharSprite2.Hframes + animClock;
+			}
 			return;
 		}
 
 		//is character allowed to turn
 		if (lockDirection){
-			if (previousFrameY == 0)
+			if (previousFrameY == 0){
+				 
 				CharSprite1.Frame = animClock;
+				CharSprite2.Frame = animClock;
+				}
 			else
+				 
 				CharSprite1.Frame = previousFrameY * CharSprite1.Hframes + animClock;
+				CharSprite2.Frame = previousFrameY * CharSprite2.Hframes + animClock;
 			return;
 		}
 
 		// find direction
 		if (direction.X > -0.55f && direction.X < 0.55f){  // up down
 				CharSprite1.FlipH = false;
+				CharSprite2.FlipH = false;
 			if (direction.Y > 0.2f){ // down
+				if (isSprite1) {
 				CharSprite1.Frame = animClock;
+				CharSprite2.Frame = 0;
+				} else {
+				CharSprite1.Frame = 0;
+				CharSprite2.Frame = animClock;
+				}
 			} else if (direction.Y < -0.2f){ // up
-				CharSprite1.Frame = 4 * CharSprite1.Hframes + animClock;
+				if (isSprite1) {
+					CharSprite1.Frame = 4 * CharSprite1.Hframes + animClock;
+					CharSprite2.Frame = 4 * CharSprite2.Hframes;
+				} else {
+					CharSprite1.Frame = 4 * CharSprite1.Hframes;
+					CharSprite2.Frame = 4 * CharSprite2.Hframes + animClock;
+				}
 			}
 		} else if (direction.X < -0.55f){ // left sides
 				CharSprite1.FlipH = false;
+				CharSprite2.FlipH = false;
 
 			if (direction.Y > 0.2f){ // downLeft
-				CharSprite1.Frame = CharSprite1.Hframes + animClock;
 
+				if (isSprite1){
+					CharSprite1.Frame = CharSprite1.Hframes + animClock;
+					CharSprite2.Frame = CharSprite2.Hframes;
+				} else {
+					CharSprite1.Frame = CharSprite1.Hframes;
+					CharSprite2.Frame = CharSprite2.Hframes + animClock;
+				}
 			} else if (direction.Y < -0.2f){ // upLeft
-				CharSprite1.Frame = 3 * CharSprite1.Hframes + animClock;
 
+				if (isSprite1){
+				CharSprite1.Frame = 3 * CharSprite1.Hframes + animClock;
+				CharSprite2.Frame = 3 * CharSprite2.Hframes;
+				} else {
+				CharSprite1.Frame = 3 * CharSprite1.Hframes;
+				CharSprite2.Frame = 3 * CharSprite2.Hframes + animClock;
+				}
 			} else { // Left
+			
+				if (isSprite1) {
 				CharSprite1.Frame = 2 * CharSprite1.Hframes + animClock;
+				CharSprite2.Frame = 2 * CharSprite2.Hframes;
+				} else {
+				CharSprite1.Frame = 2 * CharSprite1.Hframes;
+				CharSprite2.Frame = 2 * CharSprite2.Hframes + animClock;
+				}
 			}
 
 		} else if (direction.X > 0.55f){ // right sides
 				CharSprite1.FlipH = true;
+				CharSprite2.FlipH = true;
 			if (direction.Y > 0.2f){ // downRight
+			
+				if (isSprite1) {
 				CharSprite1.Frame = CharSprite1.Hframes + animClock;
+				CharSprite2.Frame = CharSprite2.Hframes;
+				} else {
+				CharSprite1.Frame = CharSprite1.Hframes;
+				CharSprite2.Frame = CharSprite2.Hframes + animClock;
+				}
 			
 			} else if (direction.Y < -0.2f){ // upRight
+				
+				if (isSprite1){
 				CharSprite1.Frame = 3 * CharSprite1.Hframes + animClock;
-			
+				CharSprite2.Frame = 3 * CharSprite2.Hframes;
+				} else {
+				CharSprite1.Frame = 3 * CharSprite1.Hframes;
+				CharSprite2.Frame = 3 * CharSprite2.Hframes + animClock;
+				}
 			} else { // Right
+				 
+				if (isSprite1){
 				CharSprite1.Frame = 2 * CharSprite1.Hframes + animClock;
+				CharSprite2.Frame = 2 * CharSprite2.Hframes;
+				} else {
+				CharSprite1.Frame = 2 * CharSprite1.Hframes;
+				CharSprite2.Frame = 2 * CharSprite2.Hframes + animClock;
+				}
 			}
 		}
-		previousFrameY = CharSprite1.FrameCoords.Y;
+		previousFrameY = setSprite.FrameCoords.Y;
 	}
 	void SetAnim(ActionStateType type){
 		// Changes the amount of Horizontal frames/sets the length of the animation
 		// each spritesheet has 5 sets one for each direction this never changes
 
 		if (CharType == CharacterType.Dragon){
+			if (type == ActionStateType.general) type = ActionStateType.draIdle;
+			
+			// Sprite to Alternate
+			Sprite2D setSprite = CharSprite1;
 
 			switch(type) {
 				case ActionStateType.draIdle:
-
-					GD.Print("idle called!");
 					
 					//DEBUG
 					// remove this once an idle anim is set
-					lockDirection = false;
-					actionType = ActionStateType.draWalk;
-					CharSprite1.Texture = GD.Load<Texture2D>(_ROOT_PATH + "/Dragon/Walk/walk_draBody1.PNG");
-					CharSprite1.Hframes = 20;
+
+					if (actionType != ActionStateType.draIdle){
+						actionType = ActionStateType.draIdle;
+
+						CharSprite1.Visible = false;
+						CharSprite2.Visible = false;
+						// Alternate Sprite
+						if (isSprite1){
+							isSprite1 = false;
+							setSprite = CharSprite2;
+						} else {
+							isSprite1 = true;
+							setSprite = CharSprite1;
+						}
+
+						lockDirection = false;
+						spriteSwapped = true;
+						setSprite.Texture = GD.Load<Texture2D>(_ROOT_PATH + "/Dragon/Walk/walk_draBody1.PNG");
+						setSprite.Hframes = 20;
+
+						if (isSprite1 == false){
+							
+							CharSprite1.Visible = false;
+							CharSprite2.Visible = true;
+						} else {
+							
+							CharSprite2.Visible = false;
+							CharSprite1.Visible = true;
+						}
+					}
 					//DEBUG
 
 					break;
 				case ActionStateType.draWalk:
 
-					GD.Print("Walk called.");
+					if (actionType != ActionStateType.draWalk){
+						actionType = ActionStateType.draWalk;
 
-					lockDirection = false;
-					actionType = ActionStateType.draWalk;
-					CharSprite1.Texture = GD.Load<Texture2D>(_ROOT_PATH + "/Dragon/Walk/walk_draBody1.PNG");
-					CharSprite1.Hframes = 20;
+						CharSprite1.Visible = false;
+						CharSprite2.Visible = false;
+						// Alternate Sprite
+						if (isSprite1){
+							isSprite1 = false;
+							setSprite = CharSprite2;
+						} else {
+							isSprite1 = true;
+							setSprite = CharSprite1;
+						}
+
+						lockDirection = false;
+						spriteSwapped = true;
+						setSprite.Texture = GD.Load<Texture2D>(_ROOT_PATH + "/Dragon/Walk/walk_draBody1.PNG");
+						setSprite.Hframes = 20;
+
+						if (isSprite1 == false){
+							CharSprite1.Visible = false;
+							CharSprite2.Visible = true;
+						} else {
+							CharSprite2.Visible = false;
+							CharSprite1.Visible = true;
+						}
+					}
 					break;
 				case ActionStateType.draBite:
 
-					GD.Print("Bite called.");
+					if (actionType != ActionStateType.draBite){
+						actionType = ActionStateType.draBite;
 
-					animClock = 0;
-					lockDirection = true;
-					actionType = ActionStateType.draBite;
-					CharSprite1.Texture = GD.Load<Texture2D>(_ROOT_PATH + "/Dragon/Bite/bite_draBody1.png");
-					CharSprite1.Hframes = 10;
+						CharSprite1.Visible = false;
+						CharSprite2.Visible = false;
+
+						// Alternate Sprite
+						if (isSprite1){
+							isSprite1 = false;
+							setSprite = CharSprite2;
+						} else {
+							isSprite1 = true;
+							setSprite = CharSprite1;
+						}
+
+						animClock = 0;
+						lockDirection = true;
+						spriteSwapped = true;
+						setSprite.Texture = GD.Load<Texture2D>(_ROOT_PATH + "/Dragon/Bite/bite_draBody1.png");
+						setSprite.Hframes = 10;
+
+						if (isSprite1 == false){
+							CharSprite1.Visible = false;
+							CharSprite2.Visible = true;
+						} else {
+							CharSprite2.Visible = false;
+							CharSprite1.Visible = true;
+						}
+					}
 					break;
 				case ActionStateType.draClaw:
-
-					GD.Print("Claw called.");
 					
-					animClock = 0;
-					lockDirection = true;
-					actionType = ActionStateType.draClaw;
-					CharSprite1.Hframes = 20;
+					if (actionType != ActionStateType.draClaw){
+						actionType = ActionStateType.draClaw;
+
+						CharSprite1.Visible = false;
+						CharSprite2.Visible = false;
+						// Alternate Sprite
+						if (isSprite1){
+							isSprite1 = false;
+							setSprite = CharSprite2;
+						} else {
+							isSprite1 = true;
+							setSprite = CharSprite1;
+						}
+
+						animClock = 0;
+						lockDirection = true;
+						spriteSwapped = true;
+						setSprite.Texture = GD.Load<Texture2D>(_ROOT_PATH + "/Dragon/Claw/claw_draBody1.png");
+						setSprite.Hframes = 20;
+
+						if (isSprite1 == false){
+							CharSprite1.Visible = false;
+							CharSprite2.Visible = true;
+						} else {
+							CharSprite2.Visible = false;
+							CharSprite1.Visible = true;
+						}
+					}
 					break;
 			}
 
@@ -289,12 +474,20 @@ public partial class Player : CharacterBody2D
 public enum ActionStateType {
 		none,
 
+		//for any type of character to return to idle
+		general,
+
 		// (dra)gon
 		draIdle,
 		draWalk,
 		draRun,
 		draBite,
 		draClaw
+
+		// (p1) playertype 1
+
+		// (p2) playertype 2
+
 	};
 	public enum AnimType {
 		none,
